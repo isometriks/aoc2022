41a9e67ddf10f5721f0ec6cd30473456677cee9f
Craig Blanchette <craig.blanchette@gmail.com>
2022-12-02 10:26:38 -0500

    idk some formatting I guess

--- lib/Day1/answer.ex
+++ lib/Day1/answer.ex
@@ -9,19 +9,19 @@ defmodule Day1 do
     |> Enum.chunk_by(fn item -> item == "" end)
     |> Enum.reject(fn list -> list == [""] end)
     |> Enum.map(&sum/1)
-    |> Enum.sort
-    |> Enum.reverse
+    |> Enum.sort()
+    |> Enum.reverse()
   end
 
   def solve2 do
     solve1()
     |> Enum.take(3)
-    |> Enum.sum
+    |> Enum.sum()
   end
 
   defp sum(calories) do
     calories
     |> Enum.map(&String.to_integer/1)
-    |> Enum.sum
+    |> Enum.sum()
   end
 end
